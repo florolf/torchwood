1cd0a72f2880a9fae0eb4e5d20c5c050d00e9916
Florian Larysch <larysch@fixme.gmbh>
2026-03-28 00:25:47 +0100

    allow large requests

--- cmd/litebastion/litebastion.go
+++ cmd/litebastion/litebastion.go
@@ -179,7 +179,7 @@ func main() {
 		}
 		hs := &http.Server{
 			Addr:         *listenHTTP,
-			Handler:      http.MaxBytesHandler(mux, 10*1024),
+			Handler:      http.MaxBytesHandler(mux, 10*1024*1024),
 			ReadTimeout:  5 * time.Second,
 			WriteTimeout: 5 * time.Second,
 		}
@@ -218,7 +218,7 @@ func main() {
 
 	hs := &http.Server{
 		Addr:         *listenAddr,
-		Handler:      http.MaxBytesHandler(mux, 10*1024),
+		Handler:      http.MaxBytesHandler(mux, 10*1024*1024),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		TLSConfig: &tls.Config{
